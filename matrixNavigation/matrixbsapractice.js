db36f34bec7929ad0dc0dbcc264b99be8727ad79
pgcr71 <34126474+pgcr71@users.noreply.github.com>
2024-07-26 08:45:11 +0000

    changes

--- matrixNavigation/matrixbsapractice.js
+++ matrixNavigation/matrixbsapractice.js
@@ -4,63 +4,60 @@ let drow = [0, -1, 0, 1];
 let dcol = [1, 0, -1, 0];
 
 let nav = [[0, 0]];
-class Cell  {
-    constructor(x, y, dist, prev) {
-        this.x = x;
-        this.y = y;
-        this.dist = dist; //distance
-        this.prev = prev; //parent cell in the path
-    }
+class Cell {
+  path;
+  parent = [];
+  visited = false;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y; //parent cell in the path
+  }
 
-	toString() {
-		return "(" + this.x + ", " + this.y + ")";
-	}
+  toString() {
+    return "(" + this.x + ", " + this.y + ")";
+  }
 }
 
 var matrix = [
-    [1, 0, 1, 1, 1],
-    [1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1],
-    [0, 0, 0, 1, 1]
+  [1, 0, 1, 1, 1],
+  [1, 0, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [0, 0, 0, 1, 1],
 ];
-let count=0;
-const visited = new Array(matrix.length).fill(0).map(() => new Array(matrix[0].length).fill(new Cell()))
-let ind = []
-for(let i=0; i< matrix.length; i++) {
-    ind.push([])
-    for (let j=0; j<matrix[0].length; j++) {
-        ind[i].push(`${i}-${j}`)
-    }
-}
-console.log(ind)
+let count = 0;
+const cells = new Array(matrix.length)
+  .fill(0)
+  .map((_, i) =>
+    new Array(matrix[0].length).fill(0).map((_, j) => new Cell(i, j)),
+  );
 
 function isValid(row, column) {
-    return (row >= 0) && (column >= 0) && (row < matrix.length) && (column < matrix[0].length) && (matrix[row][column] === 1) && !visited[row][column]
+  return (
+    row >= 0 &&
+    column >= 0 &&
+    row < matrix.length &&
+    column < matrix[0].length &&
+    matrix[row][column] === 1
+  );
 }
 
-visited[0][0] = true;
+cells[0][0].visited = true;
+cells[0][0].path = "(0,0)";
 while (nav.length !== 0) {
-    let lastElem = nav.shift();
-    if (lastElem[0] === matrix.length - 1 && lastElem[1] == matrix[0].length - 1) {
-        
+  const lastElem = nav.shift();
+  let rowIndex = lastElem[0];
+  let columnIndex = lastElem[1];
+  for (let i = 0; i < drow.length; i++) {
+    let nextRow = rowIndex + drow[i];
+    let nextCol = columnIndex + dcol[i];
+    if (isValid(nextRow, nextCol)) {
+      cells[nextRow][nextCol].visited = true;
+      cells[nextRow][nextCol].parent.push(cells[lastElem[0]][lastElem[1]]);
+      cells[nextRow][nextCol].path =
+        `${cells[rowIndex][columnIndex].path}-(${nextRow}, ${nextCol})`;
+      nav.push([nextRow, nextCol]);
     }
-        
-    let rowIndex = lastElem[0];
-    let columnIndex = lastElem[1];
-    let divergencies = 0;
-    for (let i = 0; i < drow.length; i++) {
-        let nextRow = rowIndex + drow[i];
-        let nextCol = columnIndex + dcol[i];
-        let isValid1 = isValid(nextRow, nextCol);
-
-        if (isValid1) {
-            divergencies++;
-            visited[nextRow][nextCol] = true;
-            nav.push([nextRow, nextCol])
-        }
-        for(let i=0; i<divergencies; i++) {
-            paths
-        }
-    }
-
+  }
 }
+
+console.log(cells[matrix.length - 1][matrix[0].length - 1]);
