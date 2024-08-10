4ad555cab34308d18c2c9d6361543515efa8064e
pgcr71 <34126474+pgcr71@users.noreply.github.com>
2024-08-10 15:24:23 +0530

    Time: 152 ms (97.26%), Space: 68.2 MB (87.59%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.ts
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.ts
@@ -1,44 +1,50 @@
-const dict = "abcdefghijklmnopqrstuvwxyz";
-const charMap = new Map([...dict].map((v, i) => [v, i]));
-type TrieNode = TrieNode[];
+
+
+
 // Every TrieNode is the array of size `dict.length`
 // You could fill the array with `null` values
 //  but this is unnecessary for JS/TS
 
 class Trie {
-  #root: TrieNode[] = Array(dict.length);
-  #ends = new WeakSet<TrieNode>();
+    #root = {};
+    #ends = new WeakSet<{}>();
+    isNullOrUndefined(value) {
+        return ((value ?? null) === null)
+    }
+    insert(word: string): void {
+        let node = this.#root;
 
-  insert(word: string): void {
-    let node = this.#root;
+        for (const ch of word) {
 
-    for (const ch of word) {
-      // move to next node and create it if necessary
-      node = node[charMap.get(ch)!] ??= Array(dict.length);
+            // move to next node and create it if necessary
+            if (!node[ch]) {
+                node[ch] = {};
+            }
+            node = node[ch];
+        }
+
+        this.#ends.add(node);
     }
 
-    this.#ends.add(node);
-  }
+    // private method to iterate over nodes
+    // returns the last node in word or null 
+    #search(word: string): {} | null {
+        let node = this.#root;
 
-  // private method to iterate over nodes
-  // returns the last node in word or null 
-  #search(word: string): TrieNode | null {
-    let node = this.#root;
+        for (const ch of word) {
+            const next = node[ch];
+            if (!next) return null;
+            node = next;
+        }
 
-    for (const ch of word) {
-      const next = node[charMap.get(ch) ?? -1];
-      if (!next) return null;
-      node = next;
+        return node;
     }
 
-    return node;
-  }
+    search(word: string): boolean {
+        return this.#ends.has(this.#search(word) ?? {});
+    }
 
-  search(word: string): boolean {
-    return this.#ends.has(this.#search(word) ?? []);
-  }
-
-  startsWith(prefix: string): boolean {
-    return this.#search(prefix) !== null;
-  }
+    startsWith(prefix: string): boolean {
+        return this.#search(prefix) !== null;
+    }
 }
