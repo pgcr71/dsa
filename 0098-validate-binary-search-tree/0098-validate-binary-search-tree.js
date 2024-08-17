520af02d574a5ff20a2aa928fd8230827e1d64e1
pgcr71 <34126474+pgcr71@users.noreply.github.com>
2024-08-17 20:57:32 +0530

    Time: 55 ms (87.72%), Space: 53.1 MB (68.18%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    return isValidBSTHelper(root, -Infinity, Infinity);
+};
+
+var isValidBSTHelper = function(node, min, max) {
+    if (node === null) {
+        return true;
+    }
+    if (node.val <= min || node.val >= max) {
+        return false;
+    }
+    return isValidBSTHelper(node.left, min, node.val) && isValidBSTHelper(node.right, node.val, max);
+};
