6da3c4237a363ab923e0a51dc4ee8d175317a35b
pgcr71 <34126474+pgcr71@users.noreply.github.com>
2024-08-18 22:11:23 +0530

    Time: 71 ms (52.83%), Space: 52.6 MB (76.06%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.js
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.js
@@ -1,23 +1,48 @@
-var reverseKGroup = function(head, k) {
-     let stack = [];
-    let newNode = new ListNode(-1);
-   ;
-    let temp = newNode;
+
     
-    while(head) {
-        console.log(head)
-        for(let i = 0; i < k && head; i++) {
-            stack.push(head);
-            head = head.next;
+    function length(head) {
+        let temp = head;
+        let count = 0;
+        while (temp !== null) {
+            count++;
+            temp = temp.next;
         }
-             console.log(head , "h")
-        if(stack.length === k) {
-            while(stack.length > 0) {
-                temp.next = stack.pop();
-                temp = temp.next;
-            }
-            temp.next = head;
-        }
+        return count;
     }
-    return newNode.next;
-};
+    
+    function reverseKGroup(head, k) {
+        
+        // Base Case : Empty or Single Node
+        if (head === null || head.next === null) {
+            return head;
+        }
+        
+        const len = length(head);
+        if (k > len) {
+            return head;
+        }
+        
+        // Step 1 : Reverse first k Nodes
+        let prev = null;
+        let curr = head;
+        let nextNode = null;
+        let count = 0;
+        
+        while (count < k) {
+            nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+            count++;
+        }
+        
+        // Step 2 : Recursive Call for remaining Nodes
+        if (curr !== null) {
+            head.next = reverseKGroup(curr, k);
+        }
+        
+        // Step 3 : Return head of modified linked list
+        return prev;
+    }
+
+
