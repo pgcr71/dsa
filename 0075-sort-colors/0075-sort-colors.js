5d217372a6708970391aa8d5b3b875b1d8ca162d
pgcr71 <34126474+pgcr71@users.noreply.github.com>
2025-11-29 16:20:05 +0530

    Time: 16 ms (4.88%), Space: 58.3 MB (5.16%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let colors = [0,1,2];
+    let startIndex = nums.length - 1;
+    let count = 0;
+    for(let color of colors) {
+      moveZeroes(color);
+      console.log(nums, count);
+    }
+
+    function moveZeroes(color) {
+        let nextAt = nums.length - 1;
+        let sum = count
+        for(let i=nums.length-1; i>=count;i--) {
+            if(nums[i] === color) {
+                sum++;
+                continue;
+            }
+            if (nums[i] !== nums[nextAt]) {
+                let temp = nums[i];
+                nums[i] = nums[nextAt];
+                nums[nextAt] = temp;
+            }
+            nextAt--;
+        }
+        count = sum;
+    }
+};
